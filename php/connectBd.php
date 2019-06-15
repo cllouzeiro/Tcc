7804633ea1b93c631225d3546fae0ce0e23e3bf1
cllouzeiro <51161964+cllouzeiro@users.noreply.github.com>
2019-06-15 15:22:35 -0300

    Alteração de conecção
    alterado de msqli para conecxão PDO

--- php/connectBd.php
+++ php/connectBd.php
@@ -1,1 +1,21 @@
-<?php    final class BDcon{                private $ht = 'localhost';                //USUARIO DE CONEXAO LOCAL       // private $us = 'phpmyadmin';        //USUARIO DE CONEXAO ONLINE        private $us = 'id9584311_root';                private $pd = '30093020';        private $bc = 'id9584311_arena';        function conectar(){            return mysqli_connect($this->ht, $this->us, $this->pd, $this->bc);        }    }?>
+<?php
+        /*
+        private $ht = 'localhost';
+        
+        //USUARIO DE CONEXAO LOCAL
+        private $us = 'phpmyadmin';
+
+        //USUARIO DE CONEXAO ONLINE
+        //private $us = 'id9584311_root';
+        
+        private $pd = '30093020';
+        private $bc = 'id9584311_arena';
+
+        function conectar(){
+            return mysqli_connect($this->ht, $this->us, $this->pd, $this->bc);
+        }
+        */
+
+        $conn = new PDO ('mysql:host=localhost;dbname=id9584311_arena', 'phpmyadmin', '30093020');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
